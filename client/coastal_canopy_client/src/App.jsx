2b4e4d3b1d6778daad82dbc7cefc2530d54c8da6
Malsha Gamage <malshagamage137@gmail.com>
2025-02-07 21:21:31 +0530

    Remove styles from App.jsx

--- client/coastal_canopy_client/src/App.jsx
+++ client/coastal_canopy_client/src/App.jsx
@@ -18,7 +18,7 @@ import "./index.css";
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col h-screen text-center">
+      <div>
         <Navbar />
         <div>
           <Routes>
