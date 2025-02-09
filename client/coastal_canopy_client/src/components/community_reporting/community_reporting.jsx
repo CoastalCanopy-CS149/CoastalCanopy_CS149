9e7f6f4a3309769a0ecbf18794ae325baee55350
Malsha Gamage <malshagamage137@gmail.com>
2025-02-09 07:21:56 +0530

    Modify community_reporting form with it's sub routes, create report_main component; the main page of the community reporting feature

--- client/coastal_canopy_client/src/components/community_reporting/community_reporting.jsx
+++ client/coastal_canopy_client/src/components/community_reporting/community_reporting.jsx
@@ -1,7 +1,18 @@
-export default function community_reporting() {
-    return(
-        <div>
-            <h1>Community Reporting</h1>
-        </div>
-    )
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom"
+import CommunityReportingMain from "./report_main"
+import ReportAnonymous from "./report_anonymous"
+import ReportIdentity from "./report_identity"
+
+export default function Reporting() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  return (
+    <Routes>
+      <Route index element={<CommunityReportingMain />} />
+      <Route path="report-anonymous" element={<ReportAnonymous location={location} navigate={navigate} />} />
+      <Route path="report-identity" element={<ReportIdentity location={location} navigate={navigate} />} />
+    </Routes>
+  )
 }
+
---
+++ client/coastal_canopy_client/src/components/community_reporting/report_main.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom"
+import background from "/imgs/community_reporting/BgComRep.jpg"
+
+export default function CommunityReportingMain() {
+  return (
+    <div
+      className="bg-cover min-h-screen flex justify-center items-center bg-fixed py-10"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className="z-10 bg-gray-50 bg-opacity-10 backdrop-blur-sm p-10 rounded-3xl w-11/12 max-w-7xl min-h-screen h-auto flex justify-center items-center">
+        <div className="max-w-2xl w-full text-center ">
+          <h1 className="text-4xl font-bold mb-6 ">Reporting Form</h1>
+          <p className="text-lg mb-8 text-gray-900 font-bold">
+            Choose whether to report anonymously to protect your identity or with information.
+          </p>
+          <div className="space-y-4">
+            <Link
+              to="report-anonymous"
+              className="block w-full bg-green-600 hover:bg-green-700 text-white rounded-full p-6 text-lg transition-colors duration-200"
+            >
+              Report Anonymously
+            </Link>
+            <Link
+              to="report-identity"
+              className="block w-full bg-green-600 hover:bg-green-700 text-white rounded-full p-6 text-lg transition-colors duration-200"
+            >
+              Report With Identity
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
