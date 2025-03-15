eb2f5f069677057706f46d110f4e133f472f9f56
Malsha Gamage <malshagamage137@gmail.com>
2025-03-15 19:08:30 +0530

    Update reports.py

--- server/app/routes/reports.py
+++ server/app/routes/reports.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, jsonify
+from ultralytics import YOLO
+
 from app.database import get_reports_collection
 import base64
 import os
@@ -10,6 +12,8 @@ reports_bp = Blueprint('reports', __name__)
 FOLDER = "reports"
 os.makedirs(FOLDER, exist_ok=True)
 
+# Load the YOLO model once when the application starts
+# model = YOLO('D:/MAL/IIT/2 year - 1 sem/SDGP/CoastalCanopy_CS149/server/app/routes/best.onnx', task="detect")
 
 @reports_bp.route('/submit-report', methods=['POST'])
 def submit_report():
@@ -21,22 +25,25 @@ def submit_report():
     # Extract Base64 image string
     image_data = data.get("image", "")
 
-    # Check if an image was provided
-    if image_data.startswith("data:image"):
-        # Extract the Base64 part (removing metadata like "data:image/jpeg;base64,")
-        _, encoded = image_data.split(",", 1)
+    # Extract the Base64 part (removing metadata like "data:image/jpeg;base64,")
+    _, encoded = image_data.split(",", 1)
 
-        # Decode the Base64 string
-        image_bytes = base64.b64decode(encoded)
+    # Decode the Base64 string
+    image_bytes = base64.b64decode(encoded)
 
-        # Define the image path
-        image_filename = f"{report_id}.jpg"  # Save as JPG
-        image_path = os.path.join(FOLDER, image_filename)
+    # Define the image path
+    image_filename = f"{report_id}.jpg"  # Save as JPG
+    image_path = os.path.join(FOLDER, image_filename)
 
-        # Save the image to the folder
-        with open(image_path, "wb") as image_file:
-            image_file.write(image_bytes)
+    # Save the image to the folder
+    with open(image_path, "wb") as image_file:
+        image_file.write(image_bytes)
 
+    # # Process the image using the YOLO model
+    # try:
+    #     detection_result = detect(image_path)
+    # except Exception as e:
+    #     return jsonify({"error": f"Failed to process image: {str(e)}"}), 500
 
     report = {
         "_id": report_id,
@@ -48,7 +55,8 @@ def submit_report():
         "imagePath": image_path,
         "latitudes": data.get("latitudes"),
         "longitudes": data.get("longitudes"),
-        "destructionType": data.get("destructionType")
+        "destructionType": data.get("destructionType"),
+        # "detection_result": detection_result
     }
 
     reports_collection = get_reports_collection()
@@ -58,6 +66,5 @@ def submit_report():
         return jsonify({"message": "Report submitted successfully!"}), 201
     else:
         return jsonify({"error": "Failed to submit report"}), 500
-
 
 
