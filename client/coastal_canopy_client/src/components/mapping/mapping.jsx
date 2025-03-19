92b2f2bde48e5a86120a5653cd37c9830043b2ff
Malsha Gamage <malshagamage137@gmail.com>
2025-03-19 22:18:16 +0530

    Update the API URL in front-end

--- client/coastal_canopy_client/src/components/mapping/mapping.jsx
+++ client/coastal_canopy_client/src/components/mapping/mapping.jsx
@@ -19,7 +19,7 @@ function Mapping() {
     const fetchMangroveLocations = async () => {
       try {
         setIsLoading(true)
-        const response = await fetch("http://localhost:5000/api/mapping/mangrove-locations")
+        const response = await fetch("https://coastalcanopy.up.railway.app/api/mapping/mangrove-locations")
 
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`)
