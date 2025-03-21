dd42441d4328e180c1bbc0edbc4a27a37c633481
Malsha Gamage <malshagamage137@gmail.com>
2025-03-21 21:11:09 +0530

    Update postinstall.sh

--- server/postinstall.sh
+++ server/postinstall.sh
@@ -2,6 +2,6 @@
 
 # Install missing dependencies for OpenCV
 apt-get update
-apt-get install -y libgl1-mesa-glx libglib2.0-0
+apt-get install -y libgl1-mesa-glx libglib2.0-0 libgl1
 
 echo "Postinstall script completed."
